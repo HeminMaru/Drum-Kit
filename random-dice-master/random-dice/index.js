d267f55183b5591ae470d21dba01e8e2e7917f8d
Hemin Maru <hemin.maru11@gmail.com>
2021-05-10 18:43:22 +0530

    deleted some directory

--- random-dice-master/random-dice/index.js
+++
@@ -1,6 +0,0 @@
-function random_img() {
-    var random=Math.floor(Math.random()*6)+1;
-    return "Images/"+random+".png"
-}
-document.querySelectorAll("img")[0].setAttribute("src",random_img());
-document.querySelectorAll("img")[1].setAttribute("src",random_img());
